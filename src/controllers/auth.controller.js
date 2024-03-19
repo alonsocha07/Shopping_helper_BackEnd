55d467326f0c667966d71bb153dd70a8a9d87811
alonsochaves <achaves@jandgsolutions.com>
2024-03-18 23:56:44 -0600

    protected routes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import {createAccesToken} from '../libs/jwt.js'
+import jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.js";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
@@ -53,7 +55,7 @@ export const login = async (req, res) => {
 
     const token = await createAccesToken({id: userFound._id})
 
-    res.cookie('token', token) 
+    res.cookie('token', token)
      res.json({
       message: "Bienvenido al sistema",
       id: userFound.id,
@@ -87,5 +89,23 @@ export const profile = async (req, res) => {
     updatedAt: userFound.updatedAt
   })
 
-  res.send('profile')
 };
+
+export const verifyToken = async (req, res) => {
+  const { token } = req.cookies;
+
+  if(!token) return res.status(401).json({message: "Unauthorized"})
+
+  jwt.verify(token, TOKEN_SECRET, async (err, user) => {
+    if (err) return res.status(401).json({message: "Unauthorized"})
+
+    const userFound = await User.findById(user.id)
+    if(!userFound) return res.status(401).json({message: "Unauthorized"})
+
+    return res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email
+    })
+  })
+}

ea1139a76361c9b950749ff657c674f0ffbb0008
alonsochaves <achaves@jandgsolutions.com>
2024-03-24 18:36:46 -0600

    netlify serverless, netlify-cli, netlify.toml

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import app from './app.js'
+/* import app from './functions/app.js'
 import { connectDB } from './db.js'
-
 
 
 connectDB()
 app.listen(3000)
-console.log('onport: 3000'); 
+console.log('onport: 3000');  */
